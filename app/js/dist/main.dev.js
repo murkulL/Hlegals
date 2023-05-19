7531dc285bd3ca757854ccf3b3151d882808adde
oleh <olegov23445@gmail.com>
2023-05-19 06:58:55 +0300

    fix

--- app/js/dist/main.dev.js
+++ app/js/dist/main.dev.js
@@ -2,6 +2,7 @@
 
 window.addEventListener('load', function () {
   menuActive();
+  buttonLanguage(); // collaps()
 });
 var menu = document.querySelector('.header__nav-items');
 var buttom = document.querySelector('.burger');
@@ -16,12 +17,32 @@ function menuActive() {
 $(document).ready(function () {
   $('.info__slider').slick({
     arrows: false,
-    dots: true
+    dots: true,
+    appendDots: '.info__slider-dots'
   });
-});
-$(document).ready(function () {
   $('.team__slider').slick({
     arrows: false,
-    dots: true
+    dots: true,
+    appendDots: '.team__slider-dots'
   });
 });
+var buttonLangu = document.querySelectorAll('.header__language-btn');
+
+function buttonLanguage() {
+  buttonLangu.forEach(function (item) {
+    item.addEventListener('click', function () {
+      item.classList.toggle('header__language-btn--active');
+    });
+  });
+} // const img = document.querySelectorAll('.totell__img')
+// function collaps(){
+//   img.forEach(function(item){
+//     item.addEventListener('click',() => {
+//       item.setAttribute('type', 'img');
+//       item.setAttribute('data-bs-toggle', 'collapse');
+//       item.setAttribute('data-bs-target', '.collapseExample');
+//       item.setAttribute('aria-expanded', 'false');
+//       item.setAttribute('aria-controls', 'collapseExample');
+//     })
+//   })
+// }
--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 window.addEventListener('load',function(){
     menuActive()
+    buttonLanguage()
+    // collaps()
 });
 
 const menu = document.querySelector('.header__nav-items')
@@ -12,20 +14,40 @@ function menuActive(){
     })
 }
 
+$(document).ready(function() {
+  $('.info__slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: '.info__slider-dots'
+  });
 
-  
+  $('.team__slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: '.team__slider-dots'
+  });
+});
 
-  $(document).ready(function() {
-    $('.info__slider').slick({
-        arrows: false,
-        dots: true
+const buttonLangu = document.querySelectorAll('.header__language-btn');
 
-      });
-  })
-  $(document).ready(function() {
-    $('.team__slider').slick({
-        arrows: false,
-        dots: true
+function buttonLanguage() {
+  buttonLangu.forEach(function(item) {
+    item.addEventListener('click', function(){
+      item.classList.toggle('header__language-btn--active');
+    })
+  });
+}
 
-      });
-  })
+// const img = document.querySelectorAll('.totell__img')
+
+// function collaps(){
+//   img.forEach(function(item){
+//     item.addEventListener('click',() => {
+//       item.setAttribute('type', 'img');
+//       item.setAttribute('data-bs-toggle', 'collapse');
+//       item.setAttribute('data-bs-target', '.collapseExample');
+//       item.setAttribute('aria-expanded', 'false');
+//       item.setAttribute('aria-controls', 'collapseExample');
+//     })
+//   })
+// }
